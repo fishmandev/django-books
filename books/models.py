2493177f962c050d0c6e3c0cb98bfd492c8c449c
Dmitriy Fishman <fishman.code@gmail.com>
2022-10-26 17:33:09 +0300

    Altered field 'title' on book

--- books/models.py
+++ books/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=64)
     description = models.TextField()
