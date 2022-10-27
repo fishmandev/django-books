5ade1cff72020bc13d5bd44f429c87195a68e976
Dmitriy Fishman <fishman.code@gmail.com>
2022-10-27 19:20:56 +0300

    Created a 'Book' form.

---
+++ books/forms.py
@@ -0,0 +1,13 @@
+import imp
+
+
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = [
+            'title',
+            'description'
+        ]
