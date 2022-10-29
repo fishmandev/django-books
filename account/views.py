5fd081dc0bd23bb19907a76b33c1f883310db455
Dmitriy Fishman <fishman.code@gmail.com>
2022-10-29 16:57:10 +0300

    Added a 'User login' page.

--- account/views.py
+++ account/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("default")
+        else:
+            messages.success(request, "Incorrect credentials, please try again.")
+            return redirect("login")
+    else:
+        return render(request, "login.html", {})
