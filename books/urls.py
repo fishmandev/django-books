dc2c67a8971e4248d7a6863c4cb7eba5392b586c
Dmitriy Fishman <fishman.code@gmail.com>
2022-11-04 21:00:57 +0200

    Added a 'my book list' route.

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,7 @@ from books import views
 
 urlpatterns = [
     path('create/', views.book_create_view, name='book_create'),
+    path('', views.book_list_view, name='book_list'),
     path('<slug:slug>/', views.book_detail_view, name='book_detail'),
     path('<int:id>/delete/', views.book_delete_view, name='book_delete'),
 ]
